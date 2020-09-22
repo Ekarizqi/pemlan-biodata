7c579ade0f9de18d3072c8bac7c51a99633bc653
Ekarizqi <eka30riski@gmail.com>
2020-09-22 20:03:44 +0700

    first commit

---
+++ biodata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("\t\tBIODATA\n\n");
+	printf("Nama		: Eka Rizqi Mar'atus Sholihah\n");
+	printf("NPM		: 19081010022\n");
+	printf("Prodi		: Teknik Informatika\n");
+	printf("Jenis Kelamin	: Perempuan\n");
+	printf("Agama		: Islam\n");
+	printf("TTL		: Tuban, 30 April 2001\n");
+	printf("Alamat		: Tuban, Jawa Timur\n");
+	printf("Email		: eka30riski@gmail.com\n");
+	printf("No. Handphone	: 081515408486\n");
+
+	return 0;
+}
